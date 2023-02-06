51cc6ee5cb2526861ebd2f518e84af429731a044
DanishAnsari5522 <ddanish1928@gmail.com>
2023-02-06 10:52:04 +0530

    Testing 3

--- src/components/KochoComp.js
+++ src/components/KochoComp.js
@@ -6,13 +6,13 @@ function KochoComp(props) {
     const auth = localStorage.getItem('token');
     const [carts, setCarts] = useState([]);
     const [item, setItem] = useState([]);
-    console.log(props);
+    // console.log(props);
     let auth1 = localStorage.getItem("id");
-    let productImage=props.productImage;
-    let productDetail=props.productDetail;
-    let productCategori=props.productCategori;
-    let productPrice=props.productPrice;
-    let postedBy=auth1;
+    let productImage = props.productImage;
+    let productDetail = props.productDetail;
+    let productCategori = props.productCategori;
+    let productPrice = props.productPrice;
+    let postedBy = auth1;
 
     const uploadcart = () => {
         fetch('https://helo-on-her-hair-api.onrender.com/cart', {
@@ -20,9 +20,9 @@ function KochoComp(props) {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body:JSON.stringify({productCategori,productDetail,productImage,productPrice,postedBy})
+            body: JSON.stringify({ productCategori, productDetail, productImage, productPrice, postedBy })
         });
-            
+
         // console.log(props.productImage);
         // console.log(props.productDetail);
         // console.log(props.productCategori);
@@ -54,23 +54,31 @@ function KochoComp(props) {
                 <Card bsPrefix className='cards'>
 
                     {/* <NavLink exact to={`/DetailProjectComp/${props.id}`} bsPrefix className="navlink"> */}
-                        <Card.Img variant="top" src={props.productImage} className="img w-100" bsPrefix />
+                    <Card.Img variant="top" src={props.productImage} className="img w-100" bsPrefix />
                     {/* </NavLink> */}
 
                     <Card.Body>
                         <Card.Title className="title" style={{ width: "108%" }}>{props.productDetail} and {props.productCategori}</Card.Title>
                         <Card.Text className="price">Rs. {props.productPrice}</Card.Text>
                         {auth ? <><Button variant="dark" onClick={() => {
-                            fetch(`https://helo-on-her-hair-api.onrender.com/v1/upload/deletecart/${props.id}`, {
+                            fetch(`https://helo-on-her-hair-api.onrender.com/v1/upload/delete/${props.id}`, {
                                 method: 'DELETE',
                                 headers: {
-                                    'Content-Type': 'application/json',
-                                },
+                                    'Content-Type': 'application/json'
+                                }
                             })
-                                .then(res => res.json())
-                            window.location.replace("/")
+                                .then(res => res.json().then(
+                                    async task => {
+                                        console.log(task);
+                                        if (task.success == false) {
+                                            alert("Error")
+                                        } else{
+                                            window.location.replace("/")
+                                        }
+                                       
+                                    }))
                         }}>Delete</Button>
-                            <NavLink exact to={`/UpdateProduct/${props.id}`} bsPrefix className="add-to-cart">
+                            <NavLink exact to={`/UpdateProduct/${props.id}`} className="add-to-cart">
                                 <Button variant="dark" style={{ marginLeft: "40px" }}>Edit</Button>
                             </NavLink>
                         </> : <Button variant="dark" onClick={ds}>Add To Cart</Button>}
